493c365b336045739538bbfe0b1e02d01fb90050
Gene H Li <ghl234@nyu.edu>
2019-09-19 22:08:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,10 +27,11 @@ end
 def reduce(array,*starting)
   if starting[0]
     value = starting[0]
+    count = 0
     else
     value = array[0]
+    count = 1
   end
-  count = 0
 
   while count < array.count do
     value =  yield(value,array[count])
