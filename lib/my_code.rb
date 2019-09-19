a80f8b0130d8575abb3825a1642c9d6d4a6e8d30
Gene H Li <ghl234@nyu.edu>
2019-09-19 22:02:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,41 @@
 # Your Code Here
+
+def map(array)
+  out = []
+  
+
+  #array.map {|n|
+  #out.push(yield(n))
+  #}
+  
+  
+  count = 0
+  
+  
+  while (count < array.count) do
+    out.push(yield(array[count]))
+  end
+    
+
+  
+  out
+
+end
+
+
+def reduce(array,*starting)
+  if starting[0]
+    value = starting[0]
+    else
+    value = 0
+  end
+  count = 0
+
+  while count < array.count-1 do
+    value = array[count] 
+    yield(array[count],array[count+1])
+    count+=1
+  end 
+ value
+end
+
